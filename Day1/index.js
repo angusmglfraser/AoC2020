ec18a09b4e2a8efb48fe605cb532a721b94ba46d
Angus Fraser <angus.fraser@mangahigh.com>
2020-12-04 11:21:26 +0000

    Solve day 1 part 2

--- Day1/index.js
+++ Day1/index.js
@@ -25,3 +25,34 @@ for (let i = 0; i < numbers.length; i++) {
     break;
   }
 }
+
+for (let i = 0; i < numbers.length; i++) {
+  let found = false;
+  const n1 = numbers[i];
+
+  const o1 = numbers.filter((x) => x !== n1);
+
+  for (let j = 0; j < o1.length; j++) {
+    const n2 = o1[j];
+
+    const o2 = o1.filter((x) => x !== n2);
+
+    for (let k = 0; k < o2.length; k++) {
+      const n3 = o2[k];
+
+      if (n1 + n2 + n3 === 2020) {
+        found = true;
+
+        console.log({ result: n1 * n2 * n3 });
+        break;
+      }
+    }
+
+    if (found) {
+      break;
+    }
+  }
+  if (found) {
+    break;
+  }
+}
