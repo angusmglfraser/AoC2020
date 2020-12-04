5917225cec9c5711722a30c8eb7efebb60f87c9c
Angus Fraser <angus.fraser@mangahigh.com>
2020-12-04 15:44:44 +0000

    Solve day 4

--- Day4/index.js
+++ Day4/index.js
@@ -47,3 +47,79 @@ passportData.forEach((passport) => {
 });
 
 console.log({ count });
+
+function strictlyValidatePasspost(obj) {
+  if (!validatePassport(obj)) {
+    return false;
+  }
+
+  // DATE VALIDATION
+  const byr = Number(obj.byr) || 0;
+
+  if (byr < 1920 || byr > 2002) {
+    return false;
+  }
+
+  const iyr = Number(obj.iyr) || 0;
+
+  if (iyr < 2010 || iyr > 2020) {
+    return false;
+  }
+
+  const eyr = Number(obj.eyr) || 0;
+
+  if (eyr < 2020 || eyr > 2030) {
+    return false;
+  }
+
+  // HEIGHT VAlIDATION
+  const { hgt } = obj;
+
+  if (hgt.match(/^\d+cm$/)) {
+    const measure = parseInt(hgt, 10);
+
+    if (measure < 150 || measure > 193) {
+      return false;
+    }
+  } else if (hgt.match(/^\d+in$/)) {
+    const measure = parseInt(hgt, 10);
+
+    if (measure < 59 || measure > 76) {
+      return false;
+    }
+  } else {
+    return false;
+  }
+
+  // COLOUR VALIDATION
+  const colorRegex = /^#(\d|[a-f]){6}$/i;
+
+  const { hcl } = obj;
+  if (!hcl.match(colorRegex)) {
+    return false;
+  }
+
+  const { ecl } = obj;
+  if (!['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(ecl)) {
+    return false;
+  }
+
+  // PID validation
+  const { pid } = obj;
+
+  if (!pid.match(/^\d{9}$/)) {
+    return false;
+  }
+
+  return true;
+}
+
+count = 0;
+
+passportData.forEach((passport) => {
+  if (strictlyValidatePasspost(passport)) {
+    count += 1;
+  }
+});
+
+console.log({ count });
